a826396ef70e72a7a4675d26543abc2b91ea9c61
Dyubko <dyubko.r@digdes.com>
2024-11-12 08:37:31 +0300

    Initial Commit

---
+++ neural_net_classifier/neural_net_classifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  neural_net_classifierApp.swift
+//  neural_net_classifier
+//
+//  Created by Richard Dzubko on 12.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct neural_net_classifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
